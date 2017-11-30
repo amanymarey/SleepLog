423ff38bf2f9813257f83814aee27e4e7c5dafef
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2017-11-30 14:29:20 -0800

    Played around with xml and java together.

--- app/src/main/java/io/github/jacobkrantz/sleeplog/activities/MainActivity.java
+++ app/src/main/java/io/github/jacobkrantz/sleeplog/activities/MainActivity.java
@@ -2,6 +2,9 @@ package io.github.jacobkrantz.sleeplog.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import io.github.jacobkrantz.sleeplog.R;
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button enterDataButton = (Button) findViewById(R.id.enterDataButton);
+        enterDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
